6f455ea283e57bf9792f5a5968257ddf305a1813
geniusLcy <im.lichenyi@gmail.com>
2021-09-07 18:11:51 +0800

    section 1 done

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,8 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define LOCK_NAME_SZ 6
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -21,12 +23,19 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  char name[LOCK_NAME_SZ]; // name of lock. For example: kmem0
+} kmem[NCPU]; // every CPU has a kmem freelist
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  // initialize the lock
+  int i;
+  for(i = 0; i < NCPU; i++){
+    snprintf(kmem[i].name, sizeof(kmem[i].name), "kmem%d", i);
+    initlock(&kmem[i].lock, kmem[i].name);
+  }
+  // Let freerange give all free memory to the CPU running freerange
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,10 +65,18 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // push to current CPU's freelist
+
+  push_off(); // turn off interrupt
+  
+  int current_cpu = cpuid();
+
+  acquire(&kmem[current_cpu].lock);
+  r->next = kmem[current_cpu].freelist;
+  kmem[current_cpu].freelist = r;
+  release(&kmem[current_cpu].lock);
+
+  pop_off(); // turn on interrupt
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +87,36 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off(); // turn off interrupt
+  
+  int current_cpu = cpuid();
+  acquire(&kmem[current_cpu].lock);
+  r = kmem[current_cpu].freelist;
+  if(r){ // current CPU has free space
+    kmem[current_cpu].freelist = r->next;
+  }else{ // current CPU donot has free space,
+    // borrow from other CPU
+    int i;
+    for(i = 0; i < NCPU; i++){
+      if(i == current_cpu){
+        // current CPU has no free space
+        continue;
+      }
+      acquire(&kmem[i].lock);
+      r = kmem[i].freelist;
+      if(r){
+        // find free space
+        kmem[i].freelist = r->next;
+        release(&kmem[i].lock);
+        break;
+      }
+      // still not find free space, loop continue
+      release(&kmem[i].lock);
+    }
+  }
+  release(&kmem[current_cpu].lock);
+
+  pop_off(); // turn on interrupt
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
